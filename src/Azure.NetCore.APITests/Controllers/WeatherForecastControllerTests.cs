03bdcdd078c0eef06d2dd45acf69b5727ad8d01b
Marek Wasielewski <m.wasielewski@outlook.com>
2022-03-16 20:41:48 +0100

    First try

---
+++ src/Azure.NetCore.APITests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Azure.NetCore.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.NetCore.APITests.Controllers
+{
+    [TestClass()]
+    public class WeatherForecastControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            var controler = new WeatherForecastController(null);
+            var result = controler.Get();
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count() >0);
+        }
+    }
+}
